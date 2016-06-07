52b7d971d811a9682799924e4450820d207a3db6
Adrian Salceanu <e@essenciary.com>
2016-06-07 08:08:53 +0200

    recommended settings in production

--- config/env/prod.jl
+++ config/env/prod.jl
@@ -1,8 +1,13 @@
 using Configuration
+using Logging
+
 const config = Config(output_length = 100, 
-                      supress_output = true, 
+                      supress_output = false, 
                       log_db = false, 
-                      log_requests = false, 
-                      log_responses = false)
+                      log_requests = true, 
+                      log_responses = true, 
+                      log_formatted = false, 
+                      log_level = Logging.ERROR, 
+                      log_verbosity = LOG_LEVEL_VERBOSITY_MINIMAL)
 
 export config
