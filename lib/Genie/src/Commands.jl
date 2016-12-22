05810fcedde20d0695f00618db2faf5d8e59f4a6
Adrian Salceanu <e@essenciary.com>
2016-12-22 09:50:40 +0100

    cleanup

--- lib/Genie/src/Commands.jl
+++ lib/Genie/src/Commands.jl
@@ -1,17 +1,19 @@
 module Commands
+using ArgParse, Configuration, Genie, Database, Generator, Tester, Toolbox, App, Migration, Logger, AppServer
 
-using ArgParse, Configuration, Genie, Database, Generator, Migration, Tester, Toolbox, App
 
-function called_command(args, key)
-    args[key] == "true" || args["s"] == key
-end
+"""
+    execute(config::Config) :: Void
 
-function execute(config::Config)
-  parsed_args = parse_commandline_args()::Dict{AbstractString, Any}
+Runs the requested Genie app command, based on the `args` passed to the script.
+"""
+function execute(config::Settings) :: Void
+  parsed_args = parse_commandline_args()::Dict{AbstractString,Any}
 
-  config.app_env = ENV["GENIE_ENV"]
-  config.server_port = parse(Int, parsed_args["server:port"])
-  config.server_workers_count = (sw = parse(Int, parsed_args["server:workers"])) > 0 ? sw : config.server_workers_count
+  Genie.config.app_env = ENV["GENIE_ENV"]
+  Genie.config.server_port = parse(Int, parsed_args["server:port"])
+  Genie.config.server_workers_count = (sw = parse(Int, parsed_args["server:workers"])) > 0 ? sw : config.server_workers_count
+  Genie.config.websocket_port = parse(Int, parsed_args["websocket:port"])
 
   if called_command(parsed_args, "db:init")
     Database.create_database()
@@ -47,39 +49,56 @@ function execute(config::Config)
   elseif called_command(parsed_args, "task:list")
     Toolbox.print_all_tasks()
   elseif parsed_args["task:run"] != nothing
-    Toolbox.run_task(parsed_args["task:run"])
+    Toolbox.run_task(check_valid_task!(parsed_args)["task:run"])
   elseif parsed_args["task:new"] != nothing
-    ! endswith(parsed_args["task:new"], "_task") && (parsed_args["task:new"] *= "_task")
-    Toolbox.new(parsed_args, config)
+    Toolbox.new(parsed_args |> check_valid_task!, config)
 
   elseif called_command(parsed_args, "test:run")
     Tester.run_all_tests(parsed_args["test:run"], config)
 
-  elseif called_command(parsed_args, "s")
-    Genie.startup(parsed_args)
+  elseif called_command(parsed_args, "websocket:start")
+    error("Not implemented!")
 
-  elseif isinteractive() || Configuration.IN_REPL
-    Logger.log("Started Genie interactive session", :info)
+  elseif called_command(parsed_args, "s") || called_command(parsed_args, "server:start")
+    AppServer.startup(Genie.config.server_port)
+
   end
+
+  nothing
 end
 
-function parse_commandline_args()
+
+"""
+    parse_commandline_args() :: Dict{AbstractString,Any}
+
+Extracts the command line args passed into the app and returns them as a `Dict`, possibly setting up defaults.
+Also, it is used by the ArgParse module to populate the command line help for the app `-h`.
+"""
+function parse_commandline_args() :: Dict{AbstractString,Any}
     settings = ArgParseSettings()
 
     settings.description = "Genie web framework CLI"
     settings.epilog = "Visit http://genieframework.com for more info"
-    settings.version = "0.6.1"
+    settings.version = string(Configuration.GENIE_VERSION)
     settings.add_version = true
 
     @add_arg_table settings begin
         "s"
             help = "starts HTTP server"
+        "--server:start"
+            help = "starts HTTP server"
         "--server:port", "-p"
             help = "HTTP server port"
             default = "8000"
         "--server:workers", "-w"
-            help = "Number of workers used by the app -- use any value greater than 0 to overwrite the config"
-            default = "0"
+            help = "Number of workers used by the app -- use any value greater than 1 to overwrite the config"
+            default = "1"
+
+        "--websocket:start"
+            help = "starts web sockets server"
+        "--websocket:port"
+            help = "web sockets server port"
+            default = "8008"
 
         "--db:init"
             help = "true -> create database and core tables"
@@ -92,8 +111,8 @@ function parse_commandline_args()
             help = "resource_name -> creates a new resource folder with all its files, ex: products"
 
         "--migration:status"
-            help = "true -> list migrations and their status"
-            default = "false"
+              help = "true -> list migrations and their status"
+              default = "false"
         "--migration:list"
             help = "alias for migration:status"
             default = "false"
@@ -116,7 +135,7 @@ function parse_commandline_args()
             help = "true -> list tasks"
             default = "false"
         "--task:new"
-            help = "task_name -> create a new task, ex: sync_files_task"
+            help = "task_name -> create a new task, ex: SyncFiles"
         "--task:run"
             help = "task_name -> run task"
 
@@ -125,7 +144,31 @@ function parse_commandline_args()
             default = "false"
     end
 
-    return parse_args(settings)
+    parse_args(settings)
+end
+
+
+"""
+    check_valid_task!(parsed_args::Dict{AbstractString,Any}) :: Dict{AbstractString,Any}
+
+Checks if the name of the task passed as the command line arg is valid task identifier -- if not, attempts to address it, by appending the "Task" suffix.
+Returns the potentially modified `parsed_args` `Dict`.
+"""
+function check_valid_task!(parsed_args::Dict{AbstractString,Any}) :: Dict{AbstractString,Any}
+  haskey(parsed_args, "task:new") && isa(parsed_args["task:new"], String) && ! endswith(parsed_args["task:new"], "Task") && (parsed_args["task:new"] *= "Task")
+  haskey(parsed_args, "task:run") && isa(parsed_args["task:run"], String) &&! endswith(parsed_args["task:run"], "Task") && (parsed_args["task:run"] *= "Task")
+
+  parsed_args
+end
+
+
+"""
+    called_command(args::Dict, key::String) :: Bool
+
+Checks whether or not a certain command was invoked by looking at the command line args.
+"""
+function called_command(args::Dict{AbstractString,Any}, key::String) :: Bool
+    args[key] == "true" || args["s"] == key
 end
 
 end
--- lib/Genie/src/Logger.jl
+++ lib/Genie/src/Logger.jl
@@ -1,28 +1,95 @@
 module Logger
 
-# using Lumberjack
-using Millboard
-using Genie
+using Lumberjack, Millboard, Genie
 
+# color mappings for logging levels -- to be used in STDOUT printing
 const colors = Dict{String,Symbol}("info" => :gray, "warn" => :yellow, "debug" => :green, "err" => :red, "error" => :red, "critical" => :magenta)
 
-function log(message, level = "info"; showst::Bool = true)
+"""
+    log(message, level = "info"; showst::Bool = true) :: Void
+    log(message::String, level::Symbol) :: Void
 
-  # Lumberjack.log(string(level), string(message))
+Logs `message` to all configured logs (STDOUT, FILE, etc) by delegating to `Lumberjack`.
+Supported values for `level` are "info", "warn", "debug", "err" / "error", "critical".
+If `level` is `error` or `critical` it will also dump the stacktrace onto STDOUT.
+
+# Examples
+```julia
+julia> Logger.log("hello")
+
+2016-12-21T18:38:09.105 - info: hello
+
+
+julia> Logger.log("hello", "warn")
+
+2016-12-21T18:38:22.461 - warn: hello
+
+
+julia> Logger.log("hello", "debug")
+
+2016-12-21T18:38:32.292 - debug: hello
+
+
+julia> Logger.log("hello", "err")
+
+2016-12-21T18:38:38.403 - err: hello
+```
+"""
+function log(message, level::String = "info"; showst::Bool = true) :: Void
+  level == "err" && (level = "error")
+
+  println()
+  Lumberjack.log(string(level), string(message))
+  println()
+
+  if (level == "critical" || level == "error") && showst
+    println()
+    stacktrace()
+  end
+
+  nothing
+end
+function log(message::String, level::Symbol) :: Void
+  log(message, level == :err ? "error" : string(level))
+end
+
+
+"""
+    self_log(message, level::Union{String,Symbol}) :: Void
+
+Basic logging function that does not rely on external logging modules (such as `Lumberjack`).
+
+# Examples
+```julia
+julia> Logger.self_log("hello", :err)
+
+err 2016-12-21T18:49:00.286
+hello
+
+julia> Logger.self_log("hello", :info)
+
+info 2016-12-21T18:49:05.068
+hello
+
+julia> Logger.self_log("hello", :debug)
+
+debug 2016-12-21T18:49:11.123
+hello
+```
+"""
+function self_log(message, level::Union{String,Symbol}) :: Void
   println()
   print_with_color(colors[string(level)], (string(level), " ", string(Dates.now()), "\n")...)
   print_with_color(colors[string(level)], string(message))
   println()
 
-  if level == "err" || level == "critical" && showst
-    println()
-    show_stacktrace()
-  end
-end
-function log(message::AbstractString, level::Symbol)
-  log(message, level == :err ? "error" : string(level))
+  nothing
 end
 
+
+"""
+
+"""
 function step_dict(dict::Dict)
   d = Dict()
   for (k, v) in dict
@@ -51,11 +118,24 @@ end
 function setup_loggers()
   configure(; modes=["debug", "info", "notice", "warn", "err", "critical", "alert", "emerg"])
   add_truck(LumberjackTruck(STDOUT, nothing, Dict{Any,Any}(:is_colorized => true)), "console")
-  add_truck(LumberjackTruck("$(Genie.config.app_env).log", nothing, Dict{Any,Any}(:is_colorized => true)), "file-logger")
+  add_truck(LumberjackTruck("$(joinpath(Genie.LOG_PATH, Genie.config.app_env)).log", nothing, Dict{Any,Any}(:is_colorized => true)), "file-logger")
 
   true
 end
 
-# setup_loggers()
+function empty_log_queue() :: Vector{Tuple{String,Symbol}}
+  for log_message in Genie.GENIE_LOG_QUEUE
+    log(log_message...)
+  end
+
+  empty!(Genie.GENIE_LOG_QUEUE)
+end
+
+macro location()
+  :(Logger.log(" in $(@__FILE__):$(@__LINE__)", :err))
+end
+
+setup_loggers()
+empty_log_queue()
 
 end
