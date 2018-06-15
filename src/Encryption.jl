03ba67b83ade71465cdad0ad78d605f94051365b
Adrian Salceanu <e@essenciary.com>
2018-06-15 22:20:46 +0200

    0.7 support

--- src/Encryption.jl
+++ src/Encryption.jl
@@ -3,7 +3,7 @@ Provides Genie with encryption and decryption capabilities.
 """
 module Encryption
 
-using Genie #, Genie.App #, Nettle
+using Genie, Genie.App, Nettle
 
 const ENCRYPTION_METHOD = "AES256"
 
@@ -13,15 +13,15 @@ const ENCRYPTION_METHOD = "AES256"
 
 Encrypts `s`.
 """
-# function encrypt(s::T)::String where T
-#   (key32, iv16) = encryption_sauce()
-#   encryptor = Encryptor(ENCRYPTION_METHOD, key32)
-
-#   Nettle.encrypt(encryptor, :CBC, iv16, add_padding_PKCS5(Vector{UInt8}(s), 16)) |> bytes2hex
-# end
 function encrypt(s::T)::String where T
-  error("Decryption disabled -- pending Nettle upgrade")
+  (key32, iv16) = encryption_sauce()
+  encryptor = Encryptor(ENCRYPTION_METHOD, key32)
+
+  Nettle.encrypt(encryptor, :CBC, iv16, add_padding_PKCS5(Vector{UInt8}(s), 16)) |> bytes2hex
 end
+# function encrypt(s::T)::String where T
+#   error("Decryption disabled -- pending Nettle upgrade")
+# end
 
 
 """
@@ -30,15 +30,15 @@ end
 Decrypts `s` (a `string` previously encrypted by Genie).
 """
 function decrypt(s::String) :: String
-  # (key32, iv16) = encryption_sauce()
-  # decryptor = Decryptor(ENCRYPTION_METHOD, key32)
-  # deciphertext = Nettle.decrypt(decryptor, :CBC, iv16, s |> hex2bytes)
+  (key32, iv16) = encryption_sauce()
+  decryptor = Decryptor(ENCRYPTION_METHOD, key32)
+  deciphertext = Nettle.decrypt(decryptor, :CBC, iv16, s |> hex2bytes)
 
-  # String(trim_padding_PKCS5(deciphertext))
+  String(trim_padding_PKCS5(deciphertext))
 end
-function decrypt(s::String) :: String
-  error("Decryption disabled -- pending Nettle upgrade")
-end
+# function decrypt(s::String) :: String
+#   error("Decryption disabled -- pending Nettle upgrade")
+# end
 
 function encryption_sauce() :: Tuple{Vector{UInt8},Vector{UInt8}}
   passwd = Genie.SECRET_TOKEN[1:32]
