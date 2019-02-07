49cdcc6b846bc455b70d66139ab66da6850e5119
Adrian Salceanu <e@essenciary.com>
2019-02-07 23:07:49 +0100

    cookie fixes

--- src/Cookies.jl
+++ src/Cookies.jl
@@ -3,24 +3,34 @@ Functionality for dealing with HTTP cookies.
 """
 module Cookies
 
-using HTTP, HttpCommon, Nullables
+using HTTP, Nullables
 using Genie, Genie.Encryption
 
 
 """
-    get(res::Response, key::Union{String,Symbol}) :: Nullable{String}
+    get(res::HTTP.Response, key::Union{String,Symbol}) :: Nullable{String}
 
 Retrieves a value stored on the cookie as `key` from the `Respose` object.
 """
-function get(res::HTTP.Response, key::Union{String,Symbol}) :: Nullable{String}
+function get(res::HTTP.Response, key::Union{String,Symbol}; encrypted::Bool = true) :: Nullable{String}
   if haskey(Dict(res.headers), "Set-Cookie")
     cookies = to_dict(res)
     if haskey(cookies, string(key))
-      return Nullable{String}(cookies[string(key)] |> Genie.Encryption.decrypt)
+      value = cookies[string(key)]
+      encrypted && (value = Genie.Encryption.decrypt(value))
+
+      return Nullable{String}(value)
     end
   end
 
   Nullable{String}()
+end
+
+
+"""
+"""
+function get!!(res::HTTP.Response, key::Union{String,Symbol}; encrypted::Bool = true) :: String
+  get(res, key, encrypted = encrypted) |> Nullables.get
 end
 
 
@@ -29,11 +39,14 @@ end
 
 Retrieves a value stored on the cookie as `key` from the `Request` object.
 """
-function get(req::HTTP.Request, key::Union{String,Symbol}) :: Nullable{String}
+function get(req::HTTP.Request, key::Union{String,Symbol}; encrypted::Bool = true) :: Nullable{String}
   if haskey(Dict(req.headers), "Cookie")
     cookies = to_dict(req)
     if haskey(cookies, string(key))
-      return Nullable{String}(cookies[string(key)] |> Genie.Encryption.decrypt)
+      value = cookies[string(key)]
+      encrypted && (value = Genie.Encryption.decrypt(value))
+
+      return Nullable{String}(value)
     end
   end
 
@@ -42,18 +55,46 @@ end
 
 
 """
-    set!(res::Response, key::Union{String,Symbol}, value::Any, attributes::Dict) :: Dict{String,HttpCommon.Cookie}
+"""
+function get!!(req::HTTP.Request, key::Union{String,Symbol}; encrypted::Bool = true) :: String
+  get(req, key, encrypted = encrypted) |> Nullables.get
+end
+
+
+"""
+"""
+function getcookies(req::HTTP.Request) :: Vector{HTTP.Cookies.Cookie}
+  HTTP.Cookies.cookies(req)
+end
+function getcookies(req::HTTP.Request, matching::String) :: Vector{HTTP.Cookies.Cookie}
+  HTTP.Cookies.readcookies(req.headers, matching)
+end
+
+
+"""
+    set!(res::HTTP.Response, key::Union{String,Symbol}, value::Any, attributes::Dict) :: Response
 
 Sets `value` under the `key` label on the `Cookie`.
 """
-function set!(res::HTTP.Response, key::Union{String,Symbol}, value::Any, attributes::Dict = Dict()) :: Vector{HTTP.Cookies.Cookie}
+function set!(res::HTTP.Response, key::Union{String,Symbol}, value::Any, attributes::Dict = Dict(); encrypted = true) :: HTTP.Response
   normalized_attrs = Dict{Symbol,Any}()
   for (k,v) in attributes
     normalized_attrs[Symbol(lowercase(string(k)))] = v
   end
-  HTTP.Cookie(string(key), string(value) |> Genie.Encryption.encrypt; normalized_attrs...)
 
-  HTTP.Cookies.readsetcookies("", "")
+  value = string(value)
+  encrypted && (value = Genie.Encryption.encrypt(value))
+  cookie = HTTP.Cookies.Cookie(string(key), value; normalized_attrs...)
+
+  headers = Dict(res.headers)
+  if haskey(headers, "Set-Cookie")
+    headers["Set-Cookie"] *= HTTP.Cookies.String(cookie, false)
+  else
+    headers["Set-Cookie"] = HTTP.Cookies.String(cookie, false)
+  end
+  res.headers = [(k => v) for (k,v) in headers]
+
+  res
 end
 
 
