6ada6ec8d5dfb88f5962ee77d2ab9857cb940983
Adrian Salceanu <e@essenciary.com>
2019-02-15 11:24:03 +0100

    new app teaks

--- files/new_app/config/initializers/searchlight.jl
+++ files/new_app/config/initializers/searchlight.jl
@@ -5,7 +5,6 @@ using SearchLight, SearchLight.QueryBuilder
 
 Core.eval(SearchLight, :(config.db_config_settings = SearchLight.Configuration.load_db_connection()))
 
-SearchLight.Loggers.setup_loggers()
 SearchLight.Loggers.empty_log_queue()
 
 if SearchLight.config.db_config_settings["adapter"] != nothing
