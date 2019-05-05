3c35c4ca2701d2b977def59a7ce7cc4816e3514d
Adrian Salceanu <e@essenciary.com>
2019-05-05 23:33:34 +0200

    small fix

--- src/genie_types.jl
+++ src/genie_types.jl
@@ -67,7 +67,7 @@ end
 Creates a `Dict` using the fields and the values of `m`.
 """
 function to_dict(m::Any) :: Dict{String,Any}
-  Dict(string(f) => getfield(m, Symbol(f)) for f in fieldnames(m))
+  Dict(string(f) => getfield(m, Symbol(f)) for f in fieldnames(typeof(m)))
 end
 function to_string_dict(m::Any; all_output::Bool = false) :: Dict{String,String}
   to_string_dict(m, fieldnames(m), all_output = all_output)
