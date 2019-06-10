5438ddd01f6808557e88b3d7860addad2bb32453
Adrian Salceanu <e@essenciary.com>
2019-06-10 11:46:47 +0200

    small fixes and deps updates

--- src/Plugins.jl
+++ src/Plugins.jl
@@ -17,29 +17,31 @@ const FOLDERS = [ joinpath(path_prefix, APP_FOLDER),
                   joinpath(path_prefix, TASKS_FOLDER) ]
 
 
-function recursive_copy(path::String, dest::String)
+function recursive_copy(path::String, dest::String; only_hidden = true, force = false)
   for (root, dirs, files) in walkdir(path)
-    dest_path = joinpath(dest, FILES_FOLDER, replace(root, (path_prefix * "/")=>""))
+    dest_path = joinpath(dest, replace(root, (path_prefix * "/")=>""))
 
     try
       mkdir(dest_path)
+      log("Created dir $dest_path", :info)
     catch ex
-      log(ex, :err)
+      log("Failed to create dir $dest_path", :err)
     end
 
     for f in files
-      startswith(f, ".") || continue # only copy hidden files, especially .gitkeep
+      (only_hidden && startswith(f, ".")) || continue # only copy hidden files, especially .gitkeep
       try
-        cp(joinpath(root, f), joinpath(dest_path, f))
+        cp(joinpath(root, f), joinpath(dest_path, f), force = force)
+        log("Copied $(joinpath(root, f)) to $(joinpath(dest_path, f))", :info)
       catch ex
-        log(ex, :err)
+        log("Failed to copy $(joinpath(root, f)) to $(joinpath(dest_path, f))", :err)
       end
     end
   end
 end
 
 
-function scaffold(plugin_name::String, dest::String)
+function scaffold(plugin_name::String, dest::String; force = false)
   plugin_name = replace(plugin_name, " "=>"") |> strip |> string
   dest = normpath(dest)
   ispath(dest) || mkpath(dest)
@@ -50,10 +52,10 @@ function scaffold(plugin_name::String, dest::String)
   dest = joinpath(dest, plugin_name)
 
   log("Scaffolding file structure", :info)
-  mkdir(joinpath(dest, "files"))
+  mkdir(joinpath(dest, FILES_FOLDER))
 
   for path in FOLDERS
-    recursive_copy(path, dest)
+    recursive_copy(path, joinpath(dest, FILES_FOLDER), force = force)
   end
 
   touch(joinpath(dest, FILES_FOLDER, PLUGINS_FOLDER, lowercase(plugin_name) * ".jl"))
@@ -67,6 +69,29 @@ function scaffold(plugin_name::String, dest::String)
   run(`git init`)
   run(`git add .`)
   run(`git commit -am "initial commit"`)
+end
+
+
+function install(path::String, dest::String; force = false)
+  for (root, dirs, files) in walkdir(path)
+    dest_path = joinpath(dest, split(root, "/" * FILES_FOLDER * "/")[end])
+
+    try
+      mkdir(dest_path)
+      log("Created dir $dest_path", :info)
+    catch ex
+      log("Failed to create dir $dest_path", :err)
+    end
+
+    for f in files
+      try
+        cp(joinpath(root, f), joinpath(dest_path, f), force = force)
+        log("Copied $(joinpath(root, f)) to $(joinpath(dest_path, f))", :info)
+      catch ex
+        log("Failed to copy $(joinpath(root, f)) to $(joinpath(dest_path, f))", :err)
+      end
+    end
+  end
 end
 
 end
