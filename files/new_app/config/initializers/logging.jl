d9b043bf276e96d1ddd3f0ce6d3b17cc88f3f6bf
Adrian Salceanu <e@essenciary.com>
2019-08-31 00:51:56 +0200

    v0.17.0

---
+++ files/new_app/config/initializers/logging.jl
@@ -0,0 +1,11 @@
+if Genie.config.log_to_file
+  isdir(Genie.LOG_PATH) || mkpath(Genie.LOG_PATH)
+
+  DemuxLogger(
+      MinLevelLogger(
+        FileLogger(joinpath(Genie.LOG_PATH, "$(Genie.config.app_env).log"), always_flush = true, append = true), Genie.config.log_level),
+        include_current_global = true
+  )
+else
+  ConsoleLogger(stdout, Genie.config.log_level)
+end |> global_logger
