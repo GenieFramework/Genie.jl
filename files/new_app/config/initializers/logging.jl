cee4750ee2505394f7b2924d25ddf26775ad25fa
Adrian Salceanu <e@essenciary.com>
2019-09-09 19:21:18 +0200

    v0.12.2-1

--- files/new_app/config/initializers/logging.jl
+++ files/new_app/config/initializers/logging.jl
@@ -1,9 +1,13 @@
+using Genie
+using Logging, LoggingExtras
+using Dates
+
 if Genie.config.log_to_file
   isdir(Genie.LOG_PATH) || mkpath(Genie.LOG_PATH)
 
   DemuxLogger(
       MinLevelLogger(
-        FileLogger(joinpath(Genie.LOG_PATH, "$(Genie.config.app_env).log"), always_flush = true, append = true), Genie.config.log_level),
+        FileLogger(joinpath(Genie.LOG_PATH, "$(Genie.config.app_env)-$(Dates.today()).log"), always_flush = true, append = true), Genie.config.log_level),
         include_current_global = true
   )
 else
