ab4ff22c5522189c8b7f07df2d24ed1179a29ce8
Adrian Salceanu <e@essenciary.com>
2019-09-30 13:15:07 +0200

    v0.19.0

--- files/new_app/config/env/dev.jl
+++ files/new_app/config/env/dev.jl
@@ -2,7 +2,7 @@ using Genie.Configuration
 
 const config = Settings(
   server_port                     = 8000,
-  server_host                     = "127.0.0.1",
+  server_host                     = "0.0.0.0",
   log_level                       = Logging.Debug,
   log_to_file                     = false,
   server_handle_static_files      = true,
