b382826ad93de040bcc88da95edc260d5de71a04
Adrian <e@essenciary.com>
2019-11-09 14:01:23 +0100

    test update

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,12 @@
+cd(@__DIR__)
+
+using Pkg
+pkg"activate ."
+
 using Test, TestSetExtensions, SafeTestsets
 using Genie
 
 isdir(joinpath(Genie.config.path_build, Genie.Renderer.Html.BUILD_NAME)) && rm(joinpath(Genie.config.path_build, Genie.Renderer.Html.BUILD_NAME), force = true, recursive = true)
-
-cd(@__DIR__)
 
 @testset ExtendedTestSet "Genie tests" begin
   @includetests ARGS
