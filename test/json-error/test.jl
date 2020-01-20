9dc5fb551c7040dd964392ab8934ed2936a956be
Angus Moore <18376371+angusmoore@users.noreply.github.com>
2020-01-21 07:55:24 +1100

    Update json 500 error responses for refactored JSON renderer

---
+++ test/json-error/test.jl
@@ -0,0 +1,16 @@
+using Pkg
+pkg"activate ."
+
+using Genie, HTTP
+import Genie.Router: route, POST, @params
+import Genie.Requests: jsonpayload
+
+route("/json-error", method = POST) do
+  error("500, sorry")
+end
+
+Genie.AppServer.startup()
+
+@test_throws HTTP.ExceptionRequest.StatusError HTTP.request("POST", "http://localhost:8000/json-error", [("Content-Type", "application/json; charset=utf-8")], """{"greeting":"hello"}""")
+
+exit(0)
