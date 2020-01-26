efdd2048d7e04c8cf6ce9ac9f74bb5c6cde53575
Adrian <e@essenciary.com>
2020-01-26 09:26:02 +0100

    v0.25

---
+++ src/cache_adapters/FileCache.jl
@@ -0,0 +1,109 @@
+module FileCache
+
+import Revise
+import Serialization
+import Genie, Genie.Cache
+
+const SESSIONS_FOLDER = "sessions"
+
+#===#
+# IMPLEMENTATION #
+
+"""
+    tocache(key::Union{String,Symbol}, content::Any; dir::String = "") :: Nothing
+
+Persists `content` onto the file system under the `key` key.
+"""
+function tocache(key::Union{String,Symbol}, content::Any; dir::String = "") :: Nothing
+  open(cache_path(string(key), dir = dir), "w") do io
+    Serialization.serialize(io, content)
+  end
+
+  nothing
+end
+
+
+"""
+    fromcache(key::Union{String,Symbol}, expiration::Int; dir::String = "") :: Union{Nothing,Any}
+
+Retrieves from cache the object stored under the `key` key if the `expiration` delta (in seconds) is in the future.
+"""
+function fromcache(key::Union{String,Symbol}, expiration::Int; dir::String = "") :: Union{Nothing,Any}
+  file_path = cache_path(string(key), dir = dir)
+
+  ( ! isfile(file_path) || stat(file_path).ctime + expiration < time() ) && return nothing
+
+  output = open(file_path) do io
+    Serialization.deserialize(io)
+  end
+
+  output
+end
+
+
+"""
+    cache_path(key::Union{String,Symbol}; dir::String = "") :: String
+
+Computes the path to a cache `key` based on current cache settings.
+"""
+function cache_path(key::Union{String,Symbol}; dir::String = "") :: String
+  path = joinpath(Genie.config.path_cache, dir)
+  ! isdir(path) && mkpath(path)
+
+  joinpath(path, string(key))
+end
+
+
+#===#
+# INTERFACE #
+
+
+"""
+    withcache(f::Function, key::Union{String,Symbol}, expiration::Int = Genie.config.cache_duration; dir = "", condition::Bool = true)
+
+Executes the function `f` and stores the result into the cache for the duration (in seconds) of `expiration`. Next time the function is invoked,
+if the cache has not expired, the cached result is returned skipping the function execution.
+The optional `dir` param is used to designate the folder where the cache will be stored (within the configured cache folder).
+If `condition` is `false` caching will be skipped.
+"""
+function Genie.Cache.withcache(f::Function, key::Union{String,Symbol}, expiration::Int = Genie.config.cache_duration; dir::String = "", condition::Bool = true)
+  ( expiration == 0 || ! condition ) && return f()
+
+  cached_data = fromcache(Genie.Cache.cachekey(key), expiration, dir = dir)
+
+  if cached_data === nothing
+    output = f()
+    tocache(Genie.Cache.cachekey(key), output, dir = dir)
+
+    return output
+  end
+
+  cached_data
+end
+
+
+"""
+    purge(key::Union{String,Symbol}) :: Nothing
+
+Removes the cache data stored under the `key` key.
+"""
+function Genie.Cache.purge(key::Union{String,Symbol}; dir::String = "") :: Nothing
+  rm(cache_path(Genie.Cache.cachekey(key), dir = dir))
+
+  nothing
+end
+
+
+"""
+    purgeall(; dir::String = "") :: Nothing
+
+Removes all cached data.
+"""
+function Genie.Cache.purgeall(; dir::String = "") :: Nothing
+  rm(cache_path("", dir = dir), recursive = true)
+  mkpath(cache_path("", dir = dir))
+
+  nothing
+end
+
+end
---
+++ src/session_adapters/FileSession.jl
@@ -0,0 +1,93 @@
+module FileSession
+
+using Genie
+import Serialization, Logging
+
+const SESSIONS_PATH = "sessions"
+
+"""
+    write(session::Genie.Sessions.Session) :: Genie.Sessions.Session
+
+Persists the `Session` object to the file system, using the configured sessions folder and returns it.
+"""
+function write(session::Genie.Sessions.Session) :: Genie.Sessions.Session
+  if ! isdir(joinpath(SESSIONS_PATH))
+    @warn "Sessions folder $(abspath(SESSIONS_PATH)) does not exist"
+    @info "Creating sessions folder at $(abspath(SESSIONS_PATH))"
+
+    mkpath(SESSIONS_PATH)
+  end
+
+  try
+    open(joinpath(SESSIONS_PATH, session.id), "w") do (io)
+      Serialization.serialize(io, session)
+    end
+  catch ex
+    @error "Error serializing session"
+
+    rethrow(ex)
+  end
+
+  session
+end
+
+
+"""
+    read(session_id::Union{String,Symbol}) :: Union{Nothing,Genie.Sessions.Session}
+    read(session::Genie.Sessions.Session) :: Union{Nothing,Genie.Sessions.Session}
+
+Attempts to read from file the session object serialized as `session_id`.
+"""
+function read(session_id::Union{String,Symbol}) :: Union{Nothing,Genie.Sessions.Session}
+  try
+    isfile(joinpath(SESSIONS_PATH, session_id)) || return write(Genie.Sessions.Session(session_id))
+  catch ex
+    @error "Can't check session file"
+    @error ex
+
+    write(Genie.Sessions.Session(session_id))
+  end
+
+  try
+    open(joinpath(SESSIONS_PATH, session_id), "r") do (io)
+      Serialization.deserialize(io)
+    end
+  catch ex
+    @error "Can't read session"
+    @error ex
+
+    write(Genie.Sessions.Session(session_id))
+  end
+end
+
+function read(session::Genie.Sessions.Session) :: Union{Nothing,Genie.Sessions.Session}
+  read(session.id)
+end
+
+#===#
+# IMPLEMENTATION
+
+"""
+    persist(s::Session) :: Session
+
+Generic method for persisting session data - delegates to the underlying `SessionAdapter`.
+"""
+function Genie.Sessions.persist(req::Genie.Sessions.HTTP.Request, res::Genie.Sessions.HTTP.Response, params::Dict{Symbol,Any}) :: Tuple{Genie.Sessions.HTTP.Request,Genie.Sessions.HTTP.Response,Dict{Symbol,Any}}
+  write(params[Genie.PARAMS_SESSION_KEY])
+
+  req, res, params
+end
+
+
+"""
+    load(session_id::String) :: Session
+
+Loads session data from persistent storage.
+"""
+function Genie.Sessions.load(session_id::String) :: Genie.Sessions.Session
+  session = read(session_id)
+
+  session === nothing ? Genie.Sessions.Session(session_id) : (session)
+end
+
+end
