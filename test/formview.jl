789b63fb55c1595caca2ca80d486b4a6f3f338c9
Adrian <e@essenciary.com>
2020-03-11 11:50:25 +0100

    select and option fixes

---
+++ test/formview.jl
@@ -0,0 +1,13 @@
+<form action="/new" method="POST" enctype="multipart/form-data">
+  <div class="form-group">
+    <label for="exampleFormControlSelect1">Example select</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
