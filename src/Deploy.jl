2664174ceb405cdd34e0407828ede7dbfe9a1503
Adrian Salceanu <e@essenciary.com>
2021-09-22 13:52:06 +0300

    Deploy run fix

--- src/Deploy.jl
+++ src/Deploy.jl
@@ -5,7 +5,7 @@ function run(command::Cmd)
   try Base.run(command)
 
   catch ex
-    if isa(ex, IOError)
+    if isa(ex, Base.IOError)
       @error "Can not find $(command.exec[1]). Please make sure that $(command.exec[1]) is installed and accessible."
     end
 
