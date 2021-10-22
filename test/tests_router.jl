6078efd6ddf0503cd8deb4ee38d3d561edddca97
Abhimanyu Aryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2021-10-22 15:35:15 +0530

    Fixed delete route

--- test/tests_router.jl
+++ test/tests_router.jl
@@ -9,4 +9,17 @@
 
   end;
 
+  @safetestset "router_delete" begin
+    using Genie, Genie.Router
+
+    route("/hello") do
+        "hello"
+    end
+    route("/bye") do
+        "bye"
+    end
+    @test size(routes()) == (2,)
+    Router.delete!(:get_bye)
+    @test size(routes()) == (1,)
+  end;
 end;
