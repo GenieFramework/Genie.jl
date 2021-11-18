11670dec39e6b7a5441c21811d60fdc42e228726
Adrian Salceanu <e@essenciary.com>
2021-11-18 20:31:57 +0100

    No more StructType

--- files/new_app/config/initializers/searchlight.jl
+++ files/new_app/config/initializers/searchlight.jl
@@ -6,18 +6,6 @@ try
   if SearchLight.config.db_config_settings["adapter"] !== nothing
     eval(Meta.parse("using SearchLight$(SearchLight.config.db_config_settings["adapter"])"))
     SearchLight.connect()
-
-    @eval begin
-      using Genie.Renderer.Json
-
-      function Genie.Renderer.Json.JSON3.StructTypes.StructType(::Type{T}) where {T<:SearchLight.AbstractModel}
-        Genie.Renderer.Json.JSON3.StructTypes.Struct()
-      end
-
-      function Genie.Renderer.Json.JSON3.StructTypes.StructType(::Type{SearchLight.DbId})
-        Genie.Renderer.Json.JSON3.StructTypes.Struct()
-      end
-    end
   end
 catch ex
   @error ex
