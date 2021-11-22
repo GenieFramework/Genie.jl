38baec33d34b3663871509665fc45c0146e95236
melonedo <44501064+melonedo@users.noreply.github.com>
2021-11-22 20:25:30 +0800

    Fix typo (#444)

--- src/Generator.jl
+++ src/Generator.jl
@@ -588,7 +588,7 @@ Scaffolds a new Genie app, setting up the file structure indicated by the variou
 - `dbsupport::Bool`: bootstrap the files needed for DB connection setup via the SearchLight ORM
 - `mvcsupport::Bool`: adds the files used for HTML+Julia view templates rendering and working with resources
 - `dbadapter::Union{String,Symbol,Nothing} = nothing` : pass the SearchLight database adapter to be used by default
-(one of :MySQL, :SQLite, or :PostgreSQL)`. If `dbadapter` is `nothing`, an adapter will have to be selected interactivel
+(one of :MySQL, :SQLite, or :PostgreSQL). If `dbadapter` is `nothing`, an adapter will have to be selected interactivel
 at the REPL, during the app creation process.
 
 # Examples
@@ -692,7 +692,7 @@ Template for scaffolding a new Genie app suitable for nimble web services.
 - `autostart::Bool`: automatically start the app once the file structure is created
 - `dbsupport::Bool`: bootstrap the files needed for DB connection setup via the SearchLight ORM
 - `dbadapter::Union{String,Symbol,Nothing} = nothing` : pass the SearchLight database adapter to be used by default
-(one of :MySQL, :SQLite, or :PostgreSQL)`. If `dbadapter` is `nothing`, an adapter will have to be selected interactivel
+(one of :MySQL, :SQLite, or :PostgreSQL). If `dbadapter` is `nothing`, an adapter will have to be selected interactivel
 at the REPL, during the app creation process.
 """
 function newapp_webservice(path::String = "."; autostart::Bool = true, dbsupport::Bool = false, dbadapter::Union{String,Symbol,Nothing} = nothing) :: Nothing
@@ -709,7 +709,7 @@ Template for scaffolding a new Genie app suitable for MVC web applications (incl
 - `path::String`: the name of the app and the path where to bootstrap it
 - `autostart::Bool`: automatically start the app once the file structure is created
 - `dbadapter::Union{String,Symbol,Nothing} = nothing` : pass the SearchLight database adapter to be used by default
-(one of :MySQL, :SQLite, or :PostgreSQL)`. If `dbadapter` is `nothing`, an adapter will have to be selected interactivel
+(one of :MySQL, :SQLite, or :PostgreSQL). If `dbadapter` is `nothing`, an adapter will have to be selected interactivel
 at the REPL, during the app creation process.
 """
 function newapp_mvc(path::String = "."; autostart::Bool = true, dbadapter::Union{String,Symbol,Nothing} = nothing) :: Nothing
@@ -726,7 +726,7 @@ Template for scaffolding a new Genie app suitable for full stack web application
 - `path::String`: the name of the app and the path where to bootstrap it
 - `autostart::Bool`: automatically start the app once the file structure is created
 - `dbadapter::Union{String,Symbol,Nothing} = nothing` : pass the SearchLight database adapter to be used by default
-(one of :MySQL, :SQLite, or :PostgreSQL)`. If `dbadapter` is `nothing`, an adapter will have to be selected interactivel
+(one of :MySQL, :SQLite, or :PostgreSQL). If `dbadapter` is `nothing`, an adapter will have to be selected interactivel
 at the REPL, during the app creation process.
 """
 function newapp_fullstack(path::String = "."; autostart::Bool = true, dbadapter::Union{String,Symbol,Nothing} = nothing) :: Nothing
