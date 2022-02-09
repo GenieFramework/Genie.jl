848c952803b950e35677e67e208a82df4093e666
Mattia <careddumattia@gmail.com>
2022-02-09 09:19:17 +0100

    fix issue 484 (#485)
    * fix issue 484
    
    * fix post test

--- test/tests_post_data.jl
+++ test/tests_post_data.jl
@@ -24,9 +24,17 @@
   @test response.status == 200
   @test String(response.body) == "Hello"
 
+  response = HTTP.request("POST", "http://localhost:$port/", ["Content-Type" => "application/x-www-form-urlencoded"], "greeting=Hey you there")
+  @test response.status == 200
+  @test String(response.body) == "Hey you there"
+
   response = HTTP.request("GET", "http://localhost:$port/", ["Content-Type" => "application/x-www-form-urlencoded"], "greeting=Hello")
   @test response.status == 200
   @test String(response.body) == "GET"
+
+  response = HTTP.request("POST", "http://localhost:$port/data", ["Content-Type" => "application/x-www-form-urlencoded"], "fields%5B%5D=Hey you there&fields%5B%5D=&single=")
+  @test response.status == 200
+  @test String(response.body) == "Hey you there"
 
   response = HTTP.request("POST", "http://localhost:$port/data", ["Content-Type" => "application/x-www-form-urlencoded"], "fields%5B%5D=1&fields%5B%5D=2&single=3")
   @test response.status == 200
