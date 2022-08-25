aae8c9c69a1ed2d68d8c1a04b4f6140adf60027d
hhaensel <31985040+hhaensel@users.noreply.github.com>
2022-08-25 07:59:58 +0200

    change default value for basedir in add_fileroute

--- src/Assets.jl
+++ src/Assets.jl
@@ -273,7 +273,7 @@ end
 
 """
   add_fileroute(assets_config::Genie.Assets.AssetsConfig, filename::AbstractString;
-    basedir = @__DIR__,
+    basedir = pwd(),
     type::Union{Nothing, String} = nothing, 
     content_type::Union{Nothing, Symbol} = nothing,
     ext::Union{Nothing, String} = nothing, kwargs...)
@@ -297,7 +297,7 @@ Stipple.DEPS[:qdraggabletree] = draggabletree_deps
 ```
 """
 function add_fileroute(assets_config::Genie.Assets.AssetsConfig, filename::AbstractString;
-  basedir = @__DIR__,
+  basedir = pwd(),
   type::Union{Nothing, String} = nothing, 
   content_type::Union{Nothing, Symbol} = nothing,
   ext::Union{Nothing, String} = nothing, kwargs...)
