da025c1282a18d3fa84f84b347b154e930d4e016
jeremie <jeremie.desgagne.bouchard@gmail.com>
2023-01-10 01:25:58 -0500

    html docs fix

--- src/renderers/Html.jl
+++ src/renderers/Html.jl
@@ -622,8 +622,8 @@ end
 Parses and renders the HTML `viewfile`, optionally rendering it within the `layout` file. Valid file format is `.html.jl`.
 
 # Arguments
-- `viewfile::FilePath`: filesystem path to the view file as a `Renderer.FilePath`, ie `Renderer.FilePath("/path/to/file.html.jl")`
-- `layout::FilePath`: filesystem path to the layout file as a `Renderer.FilePath`, ie `Renderer.FilePath("/path/to/file.html.jl")`
+- `viewfile::FilePath`: filesystem path to the view file as a `Renderer.FilePath`, ie `Renderer.filepath("/path/to/file.html.jl")` or `path"/path/to/file.html.jl"`
+- `layout::FilePath`: filesystem path to the layout file as a `Renderer.FilePath`, ie `Renderer.FilePath("/path/to/file.html.jl")` or `path"/path/to/file.html.jl"`
 - `context::Module`: the module in which the variables are evaluated (in order to provide the scope for vars). Usually the controller.
 - `status::Int`: status code of the response
 - `headers::HTTPHeaders`: HTTP response headers
