ee54b95068c4660182adb399213b859511c53989
Adrian Salceanu <adrian@genieframework.com>
2024-05-09 19:09:57 +0200

    Fix host to work on win, fix git line end for win

--- files/new_app/config/env/prod.jl
+++ files/new_app/config/env/prod.jl
@@ -2,7 +2,7 @@ using Genie, Logging
 
 Genie.Configuration.config!(
   server_port                     = 8000,
-  server_host                     = "0.0.0.0",
+  server_host                     = (Sys.iswindows() ? "127.0.0.1" : "0.0.0.0"),
   log_level                       = Logging.Error,
   log_to_file                     = false,
   server_handle_static_files      = true, # for best performance set up Nginx or Apache web proxies and set this to false
