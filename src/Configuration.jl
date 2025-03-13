93c53ea919f25ebf55e520ed53740dab2693889b
Helmut Hänsel <helmut.haensel@gmx.de>
2025-03-13 22:23:43 +0100

    allow for user-defined buildpath

--- src/Configuration.jl
+++ src/Configuration.jl
@@ -96,7 +96,7 @@ env()::String = Genie.config.app_env
 
 Constructs the temp dir where Genie's view files are built.
 """
-buildpath()::String = Base.Filesystem.mktempdir(prefix="jl_genie_build_")
+buildpath()::String = get(ENV, "GENIE_BUILDPATH", Base.Filesystem.mktempdir(prefix="jl_genie_build_"))
 
 
 """
