317286b227652098425049f2796b99e0c1298aea
Helmut Hänsel <helmut.haensel@gmx.de>
2025-09-14 23:57:58 +0200

    make attrs_dict an OrderedDict

--- test/tests_output_script_tags.jl
+++ test/tests_output_script_tags.jl
@@ -19,12 +19,12 @@
     """<body><p>Good morning</p><script type="text/vbscript" src="foo.vb"></script></body>"""
 
   @test Html.html(filepath("views/outputscripttags.jl.html")).body |> String |> fws ==
-    """<!DOCTYPE html><html><body><p>Greetings</p><script>alert("Hello")</script><script src="foo.js"></script>
-    <script type="text/javascript">alert("Hello")</script><script src="foo.js" type="text/javascript"></script></body></html>""" |> fws
+    """<!DOCTYPE html><html><body><p>Greetings</p><script>alert("Hello")</script><script src="foo.js">
+    </script><script type="text/javascript">alert("Hello")</script><script type="text/javascript" src="foo.js"></script></body></html>""" |> fws
 
   @test Html.html(filepath("views/outputscripttags.jl.html"), layout=filepath("views/layoutscripttags.jl.html")).body |> String |> fws ==
-    """<!DOCTYPE html><html><body><h1>Layout header</h1><section><p>Greetings</p><script>alert("Hello")</script>
-    <script src="foo.js"></script><script type="text/javascript">alert("Hello")</script><script src="foo.js" type="text/javascript">
-    </script></section><footer><h4>Layout footer</h4></footer><script>alert("Hello")</script><script src="foo.js"></script>
-    <script type="text/javascript">alert("Hello")</script><script src="foo.js" type="text/javascript"></script></body></html>""" |> fws
+    """<!DOCTYPE html><html><body><h1>Layout header</h1><section><p>Greetings</p><script>alert("Hello")</script><script src="foo.js">
+    </script><script type="text/javascript">alert("Hello")</script><script type="text/javascript" src="foo.js"></script>
+    </section><footer><h4>Layout footer</h4></footer><script>alert("Hello")</script><script src="foo.js"></script>
+    <script type="text/javascript">alert("Hello")</script><script type="text/javascript" src="foo.js"></script></body></html>""" |> fws
 end
